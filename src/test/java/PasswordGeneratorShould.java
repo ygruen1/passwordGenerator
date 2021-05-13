8a31435f9bf1cfb4ac56069038e5c9d2cc9facb6
yehuda <ygruen1@gmail.com>
2021-05-12 21:28:17 -0400

    added more TDD tests

--- src/test/java/PasswordGeneratorShould.java
+++ src/test/java/PasswordGeneratorShould.java
@@ -1,36 +1,53 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordGeneratorShould {
 
+    PasswordGenerator generator;
+    String password;
+
     @BeforeEach
     void setUp() {
-        System.out.println("before test");
+        generator = new PasswordGenerator();
+        password = null;
     }
 
     @Test
-    public void trueIsTrue(){
-    assertTrue(true);
+    public void trueIsTrue() {
+        assertTrue(true);
     }
 
     @Test
-    public void generatePasswordWith5Charachters(){
-        PasswordGenerator generator = new PasswordGenerator();
-        String password = generator.generate(5);
+    public void generatePasswordWith5Charachters() {
+
+        password = generator.generate(5);
         assertEquals(5, password.length());
     }
 
     @Test
-    public void generatePasswordWith10Charachters(){
-        PasswordGenerator generator = new PasswordGenerator();
-        String password = generator.generate(10);
+    public void generatePasswordWith10Charachters() {
+        password = generator.generate(10);
         assertEquals(10, password.length());
     }
 
     @Test
-    public void generatePasswordWithAtLeast1NumberAnd1Letter(){
+    public void generatePasswordWithAtLeast1Number() {
+        password = generator.generate(10);
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(password);
+        assertTrue(matcher.find());
+    }
 
+    @Test
+    public void generatePasswordWithAtLeast1Letter() {
+        password = generator.generate(10);
+        Pattern pattern = Pattern.compile("[a-z]+");
+        Matcher matcher = pattern.matcher(password);
+        //assertTrue(matcher.find());
     }
 }
